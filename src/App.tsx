47d90d77790af6c8832f03ac48d0e73289dee663
boon2187 <fumiya.mai.ito@gmail.com>
2024-04-13 11:35:46 +0900

    Home,Reportコンポーネントの作成と、そのrouteの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Report from "./pages/Report";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/report" element={<Report />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
