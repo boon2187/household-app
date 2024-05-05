cdc603d9abf512e06264cd5a63471443321bf352
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-05 09:35:42 +0900

    Sidebarを切り出して使わなくなったMUIコンポーネントのimportをコメントアウト（最後に消す予定）

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -3,7 +3,7 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
+// import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import List from "@mui/material/List";
