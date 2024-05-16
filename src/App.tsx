162c88274cad487ef63bce0954ef0831f0a604b5
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-16 20:49:21 +0900

    Firestoreからデータを取得する部分を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -8,8 +8,36 @@ import AppLayout from "./components/layout/AppLayout";
 import { theme } from "./theme/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
+import { Transaction } from "./types";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+        // console.log(querySnapshot);
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          // console.log(doc.id, " => ", doc.data());
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction;
+        });
+
+        console.log(transactionsData);
+        setTransactions(transactionsData);
+      } catch (err) {
+        // エラー処理
+      }
+    };
+    fetchTransactions();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
