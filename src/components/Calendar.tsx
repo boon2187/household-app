7a38a18e74d2364ab67580911a3537e2f37b5674
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-19 21:27:57 +0900

    月が変わったときの処理を追加

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -4,7 +4,7 @@ import jaLocale from "@fullcalendar/core/locales/ja";
 import "../calendar.css";
 
 import React from "react";
-import { EventContentArg } from "@fullcalendar/core";
+import { DatesSetArg, EventContentArg } from "@fullcalendar/core";
 import { calculateDailyBalances } from "../utils/financeCalculations";
 import { Balance, CalendarContent, Transaction } from "../types";
 import { formatCurrency } from "../utils/formatting";
@@ -12,14 +12,15 @@ import { formatCurrency } from "../utils/formatting";
 // Propsの型定義
 interface CalendarProps {
   monthlyTransactions: Transaction[];
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>;
 }
 
-function Calendar({ monthlyTransactions }: CalendarProps) {
+function Calendar({ monthlyTransactions, setCurrentMonth }: CalendarProps) {
   // 日付ごとの集計を受けてFullCalendar用のイベントを生成する関数
-  // 引数：日付ごとの収支の集計
-  // 返り値：FullCalendar用のイベントの配列
-  // [{ start: "2024-05-21", income: 300, expense: 100, balance: 200,},
-  //  { start: "2024-05-22", income: 500, expense: 200, balance: 300,}]
+  //    引数：日付ごとの収支の集計
+  //    返り値：FullCalendar用のイベントの配列
+  //      [{ start: "2024-05-21", income: 300, expense: 100, balance: 200,},
+  //        { start: "2024-05-22", income: 500, expense: 200, balance: 300,}]
   const createCalendarEvents = (
     dailyBalances: Record<string, Balance>
   ): CalendarContent[] => {
@@ -51,6 +52,12 @@ function Calendar({ monthlyTransactions }: CalendarProps) {
     );
   };
 
+  // 月が変わったときに呼ばれる関数
+  // const handleDatesSet = (datesetInfo: DatesSetArg) => {
+  //   // console.log(datesetInfo.view.currentStart);
+  //   setCurrentMonth(datesetInfo.view.currentStart);
+  // };
+
   // 日付ごとの収支を集計
   const dailyBalances = calculateDailyBalances(monthlyTransactions);
   // FullCalendar用のイベントに変換
@@ -63,6 +70,9 @@ function Calendar({ monthlyTransactions }: CalendarProps) {
       initialView="dayGridMonth"
       events={calendaerEvents}
       eventContent={renderEventContent}
+      datesSet={(datesetInfo: DatesSetArg) =>
+        setCurrentMonth(datesetInfo.view.currentStart)
+      }
     />
   );
 }
