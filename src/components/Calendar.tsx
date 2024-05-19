9ecf16da01b840cf38b0c54999e1f7f2037ccda8
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-19 20:49:12 +0900

    FullCalendar用のイベントを作る関数を作成

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -6,7 +6,8 @@ import "../calendar.css";
 import React from "react";
 import { EventContentArg } from "@fullcalendar/core";
 import { calculateDailyBalances } from "../utils/financeCalculations";
-import { Transaction } from "../types";
+import { Balance, CalendarContent, Transaction } from "../types";
+import { formatCurrency } from "../utils/formatting";
 
 // Propsの型定義
 interface CalendarProps {
@@ -14,6 +15,26 @@ interface CalendarProps {
 }
 
 function Calendar({ monthlyTransactions }: CalendarProps) {
+  // 日付ごとの集計を受けてFullCalendar用のイベントを生成する関数
+  // 引数：日付ごとの収支の集計
+  // 返り値：FullCalendar用のイベントの配列
+  // [{ start: "2024-05-21", income: 300, expense: 100, balance: 200,},
+  //  { start: "2024-05-22", income: 500, expense: 200, balance: 300,}]
+  const createCalendarEvents = (
+    dailyBalances: Record<string, Balance>
+  ): CalendarContent[] => {
+    return Object.keys(dailyBalances).map((date) => {
+      const { income, expense, balance } = dailyBalances[date];
+      return {
+        start: date,
+        income: formatCurrency(income),
+        expense: formatCurrency(expense),
+        balance: formatCurrency(balance),
+      };
+    });
+  };
+
+  // 日付ごとの収支を集計
   const dailyBalances = calculateDailyBalances(monthlyTransactions);
   console.log(dailyBalances);
 
