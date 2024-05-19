603a0a968d238d068daebaf583b4131cae3ceeb8
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-19 12:33:40 +0900

    収入、支出、バランスというカスタムPropsを表示する関数を作成し、eventContentPorpsとして設定

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,9 +1,10 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import jaLocale from "@fullcalendar/core/locales/ja";
-// import "../calendar.css";
+import "../calendar.css";
 
 import React from "react";
+import { EventContentArg } from "@fullcalendar/core";
 
 function Calendar() {
   const events = [
@@ -15,6 +16,24 @@ function Calendar() {
       balance: 200,
     },
   ];
+
+  // eventsの中のカスタムPropsを取得する
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    return (
+      <div>
+        <div className="money" id="event-income">
+          {eventInfo.event.extendedProps.income}円
+        </div>
+        <div className="money" id="event-expense">
+          {eventInfo.event.extendedProps.expense}円
+        </div>
+        <div className="money" id="event-balance">
+          {eventInfo.event.extendedProps.balance}円
+        </div>
+      </div>
+    );
+  };
+
   return (
     <FullCalendar
       locale={jaLocale}
