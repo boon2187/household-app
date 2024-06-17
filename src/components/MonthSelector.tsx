0c6e0252782f2669a794accfa149b5ad3d521c00
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-17 23:48:49 +0900

    カレンダーの書式を変更

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -3,17 +3,28 @@ import React from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { ja } from "date-fns/locale";
 
 const MonthSelector = () => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ja}>
       <Box
         sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
       >
         <Button color={"error"} variant="contained">
           先月
         </Button>
-        <DatePicker sx={{ mx: 2 }} />
+        <DatePicker
+          label="年月を選択"
+          sx={{ mx: 2, background: "white" }}
+          views={["year", "month"]}
+          format="yyyy/MM"
+          slotProps={{
+            toolbar: {
+              toolbarFormat: "yyyy年MM月",
+            },
+          }}
+        />
         <Button color={"primary"} variant="contained">
           今月
         </Button>
