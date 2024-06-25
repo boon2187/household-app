2731088125e7c1bd74b29211fc5604dd04050b52
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-25 19:19:53 +0900

    サンプルコードを元に、とりあえずの棒グラフを作ってみた

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,7 +1,64 @@
 import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const BarChart = () => {
-  return <div>BarChart</div>;
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Bar Chart",
+      },
+    },
+  };
+
+  const labels = [
+    "2024-05-10",
+    "2024-05-15",
+    "2024-05-20",
+    "2024-05-25",
+    "2024-05-30",
+    "2024-06-05",
+    "2024-06-10",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "支出",
+        data: [100, 200, 300, 400, 500, 600, 700],
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "収入",
+        data: [1200, 600, 800, 200, 400, 200, 100],
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+  return <Bar options={options} data={data} />;
 };
 
 export default BarChart;
